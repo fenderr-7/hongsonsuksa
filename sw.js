65d1fa29436a4b486e6c69b3fa3171438ea1f3b5
fenderr-7 <phuwanet.2543@hongson.ac.th>
2026-09-23 17:25:04 +0700

    Add Homecoming PWA service worker

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME="homecoming-v2-1790159098731";
+const APP_SHELL=["./","./index.html","./manifest.json","./favicon.svg"];
+self.addEventListener("install",event=>{
+ event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)).then(()=>self.skipWaiting()));
+});
+self.addEventListener("activate",event=>{
+ event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+});
+self.addEventListener("fetch",event=>{
+ const req=event.request;
+ if(req.method!=="GET")return;
+ event.respondWith(
+  caches.match(req).then(cached=>cached||fetch(req).then(res=>{
+   const copy=res.clone();
+   if(new URL(req.url).origin===self.location.origin){caches.open(CACHE_NAME).then(cache=>cache.put(req,copy));}
+   return res;
+  }).catch(()=>cached))
+ );
+});
